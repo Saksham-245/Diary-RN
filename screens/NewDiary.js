84a8af2de6178c2264f30b0d36278050074cda0a
Saksham Mathur <Saksham-245@users.noreply.github.com>
2022-01-06 00:06:44 +0530

    Simple Version of New Diary Screen completed.

--- screens/NewDiary.js
+++ screens/NewDiary.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import {Text, View} from "react-native";
+import tailwind from "twrnc";
+import {Button, TextInput} from "react-native-paper";
 
-const NewDiary = () => {
+const NewDiary = ({navigation}) => {
+    const [text, setText] = React.useState('');
+    const [error,setError] = React.useState(false);
+
+    const saveDiary = ()=>{
+        if(text.length === 0) {
+            setError(true);
+        }else{
+            navigation.goBack();
+        }
+    }
+
     return (
-       <View>
-           <Text>New Diary</Text>
-       </View>
+        <View style={tailwind`top-10 px-2 py-2`}>
+            <TextInput label={"Type your diary here"} value={text} multiline={true} numberOfLines={24}
+                       onChangeText={text => setText(text)} error={error}/>
+            <Button mode={"contained"} style={tailwind`top-8`} onPress={saveDiary}>Save</Button>
+        </View>
     );
 };
 
