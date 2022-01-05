7b9c0cc653e6e270fdb3aa62449e13b84b956873
Saksham Mathur <Saksham-245@users.noreply.github.com>
2022-01-06 01:22:13 +0530

    Redux logic for fetching the data from the database is also completed.

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,16 @@
-import React, {useLayoutEffect, useState} from 'react';
-import {Button, Image, View} from 'react-native';
+import React, {useCallback, useEffect, useLayoutEffect, useState} from 'react';
+import {Button, Image, View,Text} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import * as AuthActions from '../store/actions/authAction';
 import {IconButton} from "react-native-paper";
 import {PRIMARY_COLOR} from "../constants/Colors";
 import tailwind from "twrnc";
+import * as DiaryAction from "../store/actions/diariesAction";
 
 const Home = ({navigation}) => {
     const dispatch = useDispatch();
     const user = useSelector(state => state.auth.user);
+    const diaries = useSelector(state => state.diaries.diaries);
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -18,12 +20,32 @@ const Home = ({navigation}) => {
                 )
             }
         })
-    })
+    });
+
+    const loadDiaries = useCallback(async ()=>{
+        try {
+            await dispatch(DiaryAction.getDiaries(user.uid));
+        } catch (e) {
+            console.log(e);
+        }
+    },[dispatch]);
+
+    useEffect(()=>{
+        navigation.addListener('focus', loadDiaries);
+        return()=>{
+            navigation.removeListener('focus', loadDiaries);
+        }
+    },[loadDiaries]);
+
+    console.log(diaries);
+
+
     return (
         <View>
             <IconButton size={50} color={PRIMARY_COLOR} icon="plus-circle" style={tailwind`absolute top-0 right-0`} onPress={() => {
                 navigation.navigate('NewDiary');
             }}/>
+
         </View>
     );
 };
--- store/actions/diariesAction.js
+++ store/actions/diariesAction.js
@@ -1,5 +1,24 @@
 import database from '@react-native-firebase/database';
 
+export const getDiaries = (id)=>{
+    return async (dispatch)=>{
+        const diaries = await database().ref(`diaries/${id}`).once('value').then(snapshot=>{
+            return snapshot.val();
+        });
+
+        const userData = [];
+        for(const key in diaries) {
+           userData.push({
+               ...diaries[key],
+               id: key
+           });
+        }
+       dispatch({
+           type: 'GET_DIARIES',
+           payload: userData
+       });
+    }
+}
 
 export const saveDiary = (id,diary)=>{
     return async dispatch=>{
