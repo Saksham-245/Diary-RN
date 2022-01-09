fd429e7842b2025921bd5ddec79dd8c3f3da48a9
Saksham Mathur <Saksham-245@users.noreply.github.com>
2022-01-09 14:52:36 +0530

    Added some padding on android and height in Ios in New Diary Screen and added a Detail Screen to see the detail of the diary they have written

---
+++ screens/Detail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const Detail = ({route,navigation}) => {
+    return (
+        <View>
+            <Text>Detail Screen</Text>
+        </View>
+    );
+};
+
+export default Detail;
