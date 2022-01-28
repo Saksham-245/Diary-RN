0562d6cd479867d64c276398ff6f8a02f45ac391
Saksham Mathur <Saksham-245@users.noreply.github.com>
2022-01-28 16:41:43 +0530

    Added a component to display weather the app is online or not.

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import React from "react";
-import {NavigationContainer} from "@react-navigation/native";
-import AuthNavigator from "./navigation/AuthNavigator";
-import DiaryNavigator from "./navigation/DiaryNavigator";
-import {useSelector} from "react-redux";
-import {Provider} from "react-native-paper";
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import AuthNavigator from './navigation/AuthNavigator';
+import DiaryNavigator from './navigation/DiaryNavigator';
+import {useSelector} from 'react-redux';
+import {Provider} from 'react-native-paper';
+import {useNetInfo} from '@react-native-community/netinfo';
+import AlertCard from './components/AlertCard';
 
 const App = () => {
-    const user = useSelector(state => state.auth.user);
+  const user = useSelector(state => state.auth.user);
+  const netInfo = useNetInfo();
 
-    return (
-        <Provider>
-            <NavigationContainer>
-                {!user ? <AuthNavigator/> : <DiaryNavigator/>}
-            </NavigationContainer>
-        </Provider>
-    )
-}
+  React.useEffect(() => {
+    renderUI();
+    return () => {
+      renderUI();
+    };
+  }, []);
+
+  const renderUI = () => {
+    if (!netInfo.isConnected) {
+      return <AlertCard />;
+    } else {
+      return (
+        <NavigationContainer>
+          {!user ? <AuthNavigator /> : <DiaryNavigator />}
+        </NavigationContainer>
+      );
+    }
+  };
+
+  return <Provider>{renderUI()}</Provider>;
+};
 
 export default App;
---
+++ components/AlertCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function AlertCard() {
+  return (
+    <View>
+      <Text style={{textAlign: 'center'}}>No internet</Text>
+    </View>
+  );
+}
+
+export default AlertCard;
